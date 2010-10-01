0269745098b398f31edfcba8b4b66ba8d95d82c8
Steven Degutis <steven@degutis.org>
2010-10-01 15:53:58 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scna5k::Application.config.secret_token = 'bf3384357a65d6b3dc26379399f32f38fbaf787fe5964e5b03a1f427f8e824561a00c2255df97b20f589e9efe2de0064fc414feb4b7659c6086b0827de588981'
