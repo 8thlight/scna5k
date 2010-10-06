cbfa7abe8f5af8088234661eec3c903a31575595
Steven Degutis <steven@degutis.org>
2010-10-06 15:59:41 -0500

    got the login thing looking nicer now

---
+++ public/javascripts/loginform.js
@@ -0,0 +1,8 @@
+Event.observe(window, 'load', function() {
+  $('loginform').hide();
+  Event.observe('loginbutton', 'click', function() {
+    $('loginbutton').hide();
+    $('loginform').show();
+    $('loginbutton').focus();
+  });
+});
