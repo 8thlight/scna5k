f8d78e876bcbd83df77cf019274c38beef5253ac
Kevin Liddle <kliddle3@yahoo.com>
2011-10-24 18:36:25 -0500

    fix stupid sorting bug

--- spec/helpers/runners_helper_spec.rb
+++ spec/helpers/runners_helper_spec.rb
@@ -6,11 +6,13 @@ describe RunnersHelper do
     runner3 = Runner.create(:name => "Numba 3 Losa", :minutes => 5, :seconds => 47.54)
     runner1 = Runner.create(:name => "Numba 1 Stunna", :minutes => 3, :seconds => 1.01)
     runner2 = Runner.create(:name => "Numba 2 Runna Upa", :minutes => 5, :seconds => 43.34)
+    runner4 = Runner.create(:name => "Numba 4 Is Awful", :minutes => 52, :seconds => 5.02)
 
     runners = sort_by_time(Runner.all)
     runners[0].should == runner1
     runners[1].should == runner2
     runners[2].should == runner3
+    runners[3].should == runner4
   end
 
 end
