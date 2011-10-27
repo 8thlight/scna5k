cbd7d6da85be5b3f5dbfbf15c78918f9d0ecede1
Kevin Liddle <kliddle3@yahoo.com>
2011-10-27 13:55:35 -0500

    oops

--- app/helpers/runners_helper.rb
+++ app/helpers/runners_helper.rb
@@ -1,7 +1,7 @@
 module RunnersHelper
 
   def sort_by_time runners
-    runners_ary = runners.collect {|runner| {runner.id => ((runner.minutes * 60) + runner.seconds)}}
+    runners_ary = runners.collect {|runner| {runner.id => get_time(runner)}}
     runners_hash = runners_ary.inject {|all, h| all.merge(h) } || {}
     sorted_runners_ary = runners_hash.sort_by {|k,v| v }.collect {|x| x.first}
     sorted_runners = []
@@ -9,6 +9,10 @@ module RunnersHelper
       sorted_runners << Runner.find(id)
     end
     return sorted_runners
+  end
+
+  def get_time runner
+    return ((runner.minutes * 60) + runner.seconds) if runner.minutes && runner.seconds
   end
 
 end
