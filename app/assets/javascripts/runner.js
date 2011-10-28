17e2095977a4b09e61134f6ecd2bca80a0be8287
Kevin Liddle <kliddle3@yahoo.com>
2011-10-28 18:24:11 -0500

    add flashing for flashes

--- app/assets/javascripts/runner.js
+++ app/assets/javascripts/runner.js
@@ -1,2 +1,10 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  flashLoop(0);
+});
+
+var flashLoop = function(count) {
+  count = count + 1;
+  if(count <= 6){
+    $("#flash-notices").fadeToggle(800, flashLoop(count));
+  };
+};
